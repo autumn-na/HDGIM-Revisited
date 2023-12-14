19fe82600c5cacb9b50715f02969c49669ac8554
Autumn Na <autumn.na.dev@gmail.com>
2023-12-14 21:11:16 +0900

    Corrected ambiguous identifiers Modified HDGIM binding method

--- hdgim.py
+++ hdgim.py
@@ -6,10 +6,10 @@ from constants import *
 class HDGIM:
     def __init__(self):
         self.dna_sequence = None  # 1-dim DNA tensor
-        self.dna_chunks = None  # 2-dim DNA tensor
+        self.dna_subsequences = None  # 2-dim DNA tensor
         self.base_hypervectors = None  # dictionary { DNA: tensor }
-        self.encoded_hypervector = None  # 2-dim double tensor
-        self.quantized_hypervector = None  # 2-dim binary tensor
+        self.encoded_hypervector = None  # 1-dim double tensor
+        self.quantized_hypervector = None  # 1-dim binary tensor
 
         self.training_label = None  # 1-dim int tensor, 0: not included | 1: included
         self.training_data = None  # 1-dim DNA tensor
@@ -24,46 +24,33 @@ class HDGIM:
         self.dna_sequence = dna.DNASequence(DNA_SEQUENCE_LENGTH)
         self.dna_sequence.randomize()
 
-    def split_dna_chunks(self):
-        self.dna_chunks = self.dna_sequence.get_subsequences(CHUNK_LENGTH)
+    def create_dna_subsequences(self):
+        self.dna_subsequences = self.dna_sequence.get_subsequences(CHUNK_LENGTH)
 
     def bind(self):
-        chunk_hypervectors = None
-        for dna_chunk in self.dna_chunks:
-            shift_count = 0
-            bound_hypervector = None
-            chunk_hypervector = None
+        chunk_hypervectors = []
+        stacked_chunk_hypervector = torch.empty(len(self.dna_subsequences), BASE_DIMENSION)
+    
+        for shift_count, dna_subsequence in enumerate(self.dna_subsequences):
+            chunk_hypervector = torch.ones(1, BASE_DIMENSION)
 
-            for _dna in dna_chunk:
-                base_hypervector = torch.unsqueeze(self.base_hypervectors[dna.DNA(_dna.item())], dim=0)
+            for _dna in dna_subsequence:
+                dna_value = dna.DNA(_dna.item())
+                base_hypervector = torch.roll(self.base_hypervectors[dna_value], shifts=shift_count, dims=0)
+                chunk_hypervector = torch.mul(chunk_hypervector, base_hypervector)
 
-                if bound_hypervector is None:
-                    bound_hypervector = base_hypervector
-                else:
-                    bound_hypervector = torch.concat((bound_hypervector, base_hypervector), dim=0)
+            chunk_hypervectors.append(chunk_hypervector)
+  
+        stacked_chunk_hypervector = torch.stack(chunk_hypervectors)
+        self.encoded_hypervector = torch.squeeze(torch.sum(stacked_chunk_hypervector, dim=0))  # bundling hypervectors
 
-            bound_hypervector = torch.roll(bound_hypervector, shifts=shift_count, dims=0)
-            shift_count += 1
-
-            if chunk_hypervector is None:
-                chunk_hypervector = bound_hypervector
-            else:
-                chunk_hypervector = torch.mul(chunk_hypervector, bound_hypervector)
-
-            if chunk_hypervectors is None:
-                chunk_hypervectors = chunk_hypervector
-            else:
-                chunk_hypervectors = torch.stack((chunk_hypervectors, chunk_hypervector), dim=0)
-
-        self.encoded_hypervector = torch.sum(chunk_hypervectors, dim=0)
-
-    def quantize(self):
+    def quantize_min_max(self):
         min_value = torch.min(self.encoded_hypervector)
         max_value = torch.max(self.encoded_hypervector)
 
         binary_width = (max_value - min_value) / BIT_PRECISION
 
-        self.encoded_hypervector = torch.floor((self.encoded_hypervector + torch.abs(min_value)) / binary_width)
+        self.quantized_hypervector = torch.floor((self.encoded_hypervector + torch.abs(min_value)) / binary_width)
 
     def create_training_data(self):
         pass
